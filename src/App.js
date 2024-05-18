224ff8f4d4249a9690b424c681bbe0139a1062ca
Arpit Verma <131401678+vermaArpitt@users.noreply.github.com>
2024-05-18 10:17:21 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
